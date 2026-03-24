d68051d6783b0837f7d012fa357dad11dc80b0b1
Leonardo <leonardolopespereira7@gmail.com>
2026-03-23 21:40:33 -0300

    feat: driving students and student

--- models/DrivingSchool.java
+++ models/DrivingSchool.java
@@ -1,15 +1,14 @@
 package models;
-import services.SchoolService;
-
 /**
- * DrivingSchool
- */
-
+* DrivingSchool
+*/
 import models.Student;
-
 public class DrivingSchool {
-    public void showSummary(Student student){
-       SchoolService service = new SchoolService();
-       service.showSummary(student);
-    }
+   public void showSummary(Student student){
+       System.out.println("Name: " + student.name);
+       System.out.println("Age: " + student.age);
+       System.out.println("Practical Classes: " + student.practicalClasses);
+       System.out.println("Theoric Classes: " + student.theoricClasses);
+       System.out.println("Is Approved: " + student.isApproved);
+   }
 }
--- models/Student.java
+++ models/Student.java
@@ -1,38 +1,80 @@
 package models;
 
-import services.StudentService;
+import java.util.Random;
 
 /**
- * Student
- */
+
+- Student
+
+  */
 
 public class Student {
-   public String name;
-   public int age;
-   public String cardType;
-   public int theoricClasses;
-   public int practicalClasses;
-   public boolean isApproved = false;
 
-   public void schedulePraticalClass(){
-        this.practicalClasses = this.practicalClasses + 1;
-   }
+public String name;
 
-   public void scheduleTheoricClass(){
-        this.theoricClasses = this.theoricClasses + 1;
-   }
+public int age;
 
-   public void cancelPraticalClass(){
-        this.practicalClasses = this.practicalClasses - 1;
-   }
+public String cardType;
 
-   public void cancelTheoricClass(){
-        this.theoricClasses = this.theoricClasses - 1;
-   }
+public int theoricClasses;
 
-   public void doExam(){
-       StudentService studentService = new StudentService();
-       boolean result = studentService.doExamService(this.theoricClasses, this.practicalClasses);
-       this.isApproved = result;
-   }
+public int practicalClasses;
+
+public boolean isApproved = false;
+
+public void schedulePraticalClass(){
+
+this.practicalClasses = this.practicalClasses + 1;
+
 }
+
+public void scheduleTheoricClass(){
+
+this.theoricClasses = this.theoricClasses + 1;
+
+}
+
+public void cancelPraticalClass(){
+
+if (this.practicalClasses > 0) {
+
+this.practicalClasses = this.practicalClasses - 1;
+
+}
+
+}
+
+public void cancelTheoricClass(){
+
+if (this.theoricClasses > 0) {
+
+this.theoricClasses = this.theoricClasses - 1;
+
+}
+
+}
+
+public void doExam(){
+
+if (this.practicalClasses >= 5 && this.theoricClasses >= 5) {
+
+Random random = new Random();
+
+int result = random.nextInt(2);
+
+if (result == 1) {
+
+this.isApproved = true;
+
+}
+
+} else {
+
+System.out.println(this.name + “ cannot take the exam. Insufficient classes.”);
+
+}
+
+}
+
+}
+ 
